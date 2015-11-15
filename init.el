f1bcd9f913b349fc93f09bacc4ad1a81077a1c8c
Hebi Li <lihebi.com@gmail.com>
2015-11-15 00:18:07 -0600

    Initial version. Working.

---
+++ init.el
@@ -0,0 +1,402 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;; Package archives ;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("org" . "http://orgmode.org/elpa/"))
+;; (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
+
+;; auto-update
+;; this should be called after adding these for the first time
+;; be CAREFUL, this will check if there exists the archive.
+;; If no, then it usually means I just clone from git.
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+; auto-install packages
+(defvar my-packages '(better-defaults magit))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; general config ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; set nu
+(global-linum-mode 1)
+;; mode line settings
+(line-number-mode t)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; ignore the bell
+(setq ring-bell-function 'ignore)
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+(setq user-full-name "Hebi Li"
+      user-mail-address "lihebi.com@gmail.com")
+
+;; this should already there in "better default" package
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+          (save-buffers-kill-terminal)
+        (save-buffers-kill-emacs))
+    (message "Canceled exit")))
+
+(when window-system
+  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
+
+
+
+
+(load-theme 'zenburn t)
+(load-theme 'ample t t)
+(load-theme 'ample-flat t t)
+(load-theme 'ample-light t t)
+(load-theme 'dracula t)
+(enable-theme 'ample)
+
+;; smooth scrolling
+;; comment out to see effect. Will turn on eventually
+;; (setq redisplay-dont-pause t
+;;       scroll-margin 1
+;;       scroll-step 1
+;;       scroll-conservatively 10000
+;;       scroll-preserve-screen-position 1)
+
+;; (setq scroll-conservatively 10)
+;; (setq scroll-margin 7)
+
+;; recent files
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+;; (global-set-key "C-x C-r" 'recentf-open-files)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; packages ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'use-package)
+
+;; not sure if these wierd binding is what I want
+(use-package windmove
+  :defer t
+  :bind
+  (("<f2> <right>" . windmove-right)
+   ("<f2> <left>" . windmove-left)
+   ("<f2> <up>" . windmove-up)
+   ("<f2> <down>" . windmove-down)
+   ))
+
+(use-package org-mode
+  :defer t
+  :bind
+  (("C-c n" . org-capture)
+   ("C-c o" . org-open-at-point)
+   )
+  :init
+  (progn
+    (defvar org-startup-folded)
+    (defvar org-directory)
+    (defvar org-capture-templates)
+    (defvar org-agenda-files)
+    (setq org-startup-folded nil)
+    (setq org-directory "~/github/org")
+    ;; capture templates
+    (setq org-capture-templates
+          '(("t" "TODO" entry (file+headline (concat org-directory "/scratch.org") "Tasks")
+             "* TODO %?\n  %U")
+            ("n" "Note" entry (file (concat org-directory "/scratch.org"))
+             "* Notes on %U\n%?" :prepend t)
+            ("s" "Stack" entry (file (concat org-directory "/stack.org"))
+             "* New Stack on %U\n%?" :prepend t)
+            ))
+    (setq org-agenda-files (list org-directory))
+    )
+  )
+
+
+
+(defun hebi/open-my-stack ()
+  (interactive)
+  (find-file (concat org-directory "/stack.org")))
+(defun hebi/open-my-init ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(defun hebi/open-my-todo ()
+  (interactive)
+  (find-file (concat org-directory "/todo.org")))
+(defun hebi/open-my-note ()
+  (interactive)
+  (find-file (concat org-directory "/notes.org")))
+(defun hebi/open-my-scratch ()
+  (interactive)
+  (find-file (concat org-directory "/scratch.org")))
+
+;; smex: use ido in M-x
+(require 'smex)
+(smex-initialize)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; powerline
+;; (require 'smart-mode-line)
+;; (setq sml/no-confirm-load-theme t)
+;; (setq sml/theme nil)
+;; (setq sml/theme 'dark)
+;; (setq sml/theme 'light)
+;; (setq sml/theme 'respectful)
+;; (setq sml/theme 'powerline)
+;; (sml/setup)
+;; not sure why use this hook instead of
+;; using the official recommanded way above
+;; (add-hook 'after-init-hook #'sml/setup) 
+
+;; {}
+;; TODO how to enable smartparens-mode on default?
+(require 'smartparens-config)
+(define-globalized-minor-mode my-global-smartparens-mode smartparens-mode
+  (lambda () (smartparens-mode t)))
+(my-global-smartparens-mode t)                    ; enable smartparens-mode by default
+
+
+;; markdown-mode
+(require 'markdown-mode)
+(add-hook 'markdown-mode-hook 'turn-on-orgtbl)
+
+;; projectile
+(projectile-global-mode)
+(setq projectile-enable-caching t) ; enable catch
+
+;; neotree
+;; neotree is not using because it conflicts with perspective
+;; also, we have speedbar ^_^
+;; (require 'neotree)
+;; (global-set-key [f8] 'neotree-toggle)
+;; keybinding
+;; (define-key neotree-mode-map (kbd "i") #'neotree-enter-horizontal-split) ; TODO what's the #?
+;; (define-key neotree-mode-map (kbd "I") #'neotree-enter-vertical-split)
+;; when switch project, neotree change root automatically
+;; (setq projectile-switch-project-action 'neotree-projectile-action)
+
+;; perspective: use for manage multiple project in a frame
+;; Do not use because when switch project this way, I cannot make neotree change accordingly.
+(persp-mode)
+;; persp-projectil: use projectile together with perspective ; this should just be install...
+;; (require 'persp-projectile)
+(define-key projectile-mode-map (kbd "s-s") 'projectile-persp-switch-project)
+
+;; fill-column-indicator
+(define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode 1)
+
+;; sublimity
+;; smooth-scrolling & minimap
+;; not good: no color. delay may need to config, not good performance. scroll is terrible.
+(require 'sublimity)
+;; (require 'sublimity-scroll)
+(require 'sublimity-map)
+(sublimity-map-set-delay nil)
+;; (require 'sublimity-attractive)
+;; (sublimity-mode 1) ; we do not open it by default because performance issue.
+
+;; company-mode
+(add-hook 'after-init-hook 'global-company-mode)
+(global-set-key (kbd "s-/") 'company-complete)
+
+;; yasnippet
+;; (yas-reload-all)
+;; (add-hook 'prog-mode-hook #'yas-minor-mode)
+(yas-global-mode 1)
+
+;; multiple-cursors
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines) ; add cursor for each line in region
+;; based on the current marked word
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click) ; mouse select
+
+;; git-gutter
+(require 'git-gutter)
+(global-git-gutter-mode t)
+(git-gutter:linum-setup)
+
+(global-set-key (kbd "C-x C-g") 'git-gutter:toggle)
+
+;; not working for live update ..
+;; (custom-set-variables
+;;  '(git-gutter:update-interval 1))
+
+(custom-set-variables
+ '(git-gutter:modified-sign "  ") ;; two space
+ '(git-gutter:added-sign "++")    ;; multiple character is OK
+ '(git-gutter:deleted-sign "--"))
+
+;; (custom-set-variables
+;;  '(git-gutter:window-width 2)
+;;  '(git-gutter:modified-sign "☁")
+;;  '(git-gutter:added-sign "☀")
+;;  '(git-gutter:deleted-sign "☂"))
+
+(set-face-background 'git-gutter:modified "purple") ; background color
+(set-face-foreground 'git-gutter:added "green") ; foreground not working ...
+(set-face-foreground 'git-gutter:deleted "red")
+
+;; flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; expand-region
+(global-set-key (kbd "s-e") 'er/expand-region)
+
+;; helm
+(require 'helm-config)
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+;; (setq helm-projectile-fuzzy-match nil)
+(require 'helm-projectile)
+(helm-projectile-on) ; enable key binding for helm-xxx
+
+;; fuzzy config for helm
+;; TODO no function?
+;; (helm-M-x-fuzzy-match t)
+
+;; helm-ag
+;; not working
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;; key bindings ;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar hebi/keys-minor-mode-map (make-keymap)
+  "My keys-minor-mode to prioritize keymap.")
+
+;; magit
+(define-key hebi/keys-minor-mode-map (kbd "C-x g") 'magit-status)
+(define-key hebi/keys-minor-mode-map (kbd "C-=") 'text-scale-increase)
+(define-key hebi/keys-minor-mode-map (kbd "C--") 'text-scale-decrease)
+(define-key hebi/keys-minor-mode-map (kbd "C-x O")
+  (lambda ()
+    (interactive)
+    (other-window -1)))
+
+;; Start eshell or switch to it if it's active.
+(define-key hebi/keys-minor-mode-map (kbd "C-x m") 'eshell)
+
+;; Start a new eshell even if one is active.
+(define-key hebi/keys-minor-mode-map (kbd "C-x M")
+  (lambda () (interactive) (eshell t)))
+
+;; Start a regular shell if you prefer that.
+(define-key hebi/keys-minor-mode-map (kbd "C-x M-m") 'shell)
+
+;; kill lines backward
+(define-key hebi/keys-minor-mode-map (kbd "C-<backspace>")
+  (lambda ()
+    (interactive)
+    (kill-line 0)
+    (indent-according-to-mode)))
+
+(define-key hebi/keys-minor-mode-map (kbd "s-<return>")
+  (lambda()
+    (interactive)
+    (move-end-of-line 1)
+    (newline)))
+
+(define-key hebi/keys-minor-mode-map (kbd "S-s-<return>")
+  (lambda()
+    (interactive)
+    (move-end-of-line 0)
+    (newline)))
+
+(define-minor-mode hebi/keys-minor-mode
+  "A minor mode so that my key settings override annoying major modes."
+  t " hebi-keys" 'hebi/keys-minor-mode-map)
+
+(hebi/keys-minor-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;; Deprecated
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; pop to mark
+;; (bind-key "C-x p" 'pop-to-mark-command)
+;; (setq set-mark-command-repeat-pop t)
+
+;; make window splitting more useful
+;; (defun my/vsplit-last-buffer (prefix)
+;;   "Split the window vertically and display the previous buffer."
+;;   (interactive "p")
+;;   (split-window-vertically)
+;;   (other-window 1 nil)
+;;   (if (= prefix 1)
+;;       (switch-to-next-buffer)))
+;; (defun my/hsplit-last-buffer (prefix)
+;;   "Split the window horizontally and display the previous buffer."
+;;   (interactive "p")
+;;   (split-window-horizontally)
+;;   (other-window 1 nil)
+;;   (if (= prefix 1) (switch-to-next-buffer)))
+;; (bind-key "C-x 2" 'my/vsplit-last-buffer)
+;; (bind-key "C-x 3" 'my/hsplit-last-buffer)
+
+;; replace buffer-menu with ibuffer
+;; already done with "better default" package
+;; (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+
+;; add hook for ido match in minibuffer
+;; (add-hook 'ido-setup-hook 'ido-my-keys)
+
+;; comment out because we can already do this by `C-,`
+;; (defun ido-my-keys ()
+;;   "Add my keybindings for ido."
+;;   (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+;;   (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
+;;   )
+
+;; comment out because this is the global mark ring!
+;; cycling space
+;; (global-set-key (kbd "C-x C-SPC") (lambda ()
+;;                                     (interactive)
+;;                                     (cycle-spacing)
+;;                                     (backward-delete-char-untabify 1)))
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; THIS IS THE END OF MY COFNIG ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
