923e616ecb3a8c88df6f9bc297922b0be3bffe2c
Hebi Li <lihebi.com@gmail.com>
2015-12-19 19:48:18 -0600

    Just use global set key for keybinding

--- bindings.el
+++ bindings.el
@@ -3,70 +3,61 @@
 
 ;;; Code:
 
-(defvar hebi/keys-minor-mode-map (make-keymap)
-  "My keys-minor-mode to prioritize keymap.")
-
-(define-key hebi/keys-minor-mode-map (kbd "C-=") 'text-scale-increase)
-(define-key hebi/keys-minor-mode-map (kbd "C--") 'text-scale-decrease)
-(define-key hebi/keys-minor-mode-map (kbd "C-x O")
-  (lambda ()
-    (interactive)
-    (other-window -1)))
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-x O")
+                (lambda ()
+                  (interactive)
+                  (other-window -1)))
 
 ;; Start eshell or switch to it if it's active.
-(define-key hebi/keys-minor-mode-map (kbd "C-x m") 'eshell)
+(global-set-key (kbd "C-x m") 'eshell)
 
 ;; Start a new eshell even if one is active.
-(define-key hebi/keys-minor-mode-map (kbd "C-x M")
-  (lambda () (interactive) (eshell t)))
+(global-set-key (kbd "C-x M")
+                (lambda () (interactive) (eshell t)))
 
 ;; Start a regular shell if you prefer that.
-(define-key hebi/keys-minor-mode-map (kbd "C-x M-m") 'shell)
+(global-set-key (kbd "C-x M-m") 'shell)
 
 ;; kill lines backward
-(define-key hebi/keys-minor-mode-map (kbd "C-<backspace>")
-  (lambda ()
-    (interactive)
-    (kill-line 0)
-    (indent-according-to-mode)))
+(global-set-key (kbd "C-<backspace>")
+                (lambda ()
+                  (interactive)
+                  (kill-line 0)
+                  (indent-according-to-mode)))
 
-(define-key hebi/keys-minor-mode-map (kbd "s-<return>")
-  (lambda()
-    (interactive)
-    (move-end-of-line 1)
-    (newline)))
+(global-set-key (kbd "s-<return>")
+                (lambda()
+                  (interactive)
+                  (move-end-of-line 1)
+                  (newline)))
 
-(define-key hebi/keys-minor-mode-map (kbd "S-s-<return>")
-  (lambda()
-    (interactive)
-    (move-end-of-line 0)
-    (newline)))
+(global-set-key (kbd "S-s-<return>")
+                (lambda()
+                  (interactive)
+                  (move-end-of-line 0)
+                  (newline)))
 
 ;; backspace
-(define-key hebi/keys-minor-mode-map (kbd "C-h")
-  (lambda ()
-    (interactive)
-    (backward-delete-char 1)))
+(global-set-key (kbd "C-h")
+                (lambda ()
+                  (interactive)
+                  (backward-delete-char 1)))
 
-(define-key hebi/keys-minor-mode-map (kbd "<f1>") 'help-command)
+(global-set-key (kbd "<f1>") 'help-command)
 
 ;; join line
-(define-key hebi/keys-minor-mode-map (kbd "C-j")
-  (lambda()
-    (interactive)
-    (join-line -1)))
+(global-set-key (kbd "C-j")
+                (lambda()
+                  (interactive)
+                  (join-line -1)))
 
 ;; other window
-(define-key hebi/keys-minor-mode-map (kbd "C-<return>") 'other-window)
-(define-key hebi/keys-minor-mode-map (kbd "<f5>") (lambda ()
-                                                    (interactive)
-                                                    (shell-command "make")
-                                                    ))
-
-(define-minor-mode hebi/keys-minor-mode
-  "A minor mode so that my key settings override annoying major modes."
-  t " hebi-keys" 'hebi/keys-minor-mode-map)
-
-(hebi/keys-minor-mode 1)
+(global-set-key (kbd "C-<return>") 'other-window)
+(global-set-key (kbd "<f5>") (lambda ()
+                               (interactive)
+                               (shell-command "make")
+                               ))
 
 ;;; bindings.el ends here
