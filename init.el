713032932f4a37cb336f3a1dda71251370a1ca7d
Hebi Li <lihebi.com@gmail.com>
2015-12-22 00:42:41 -0600

    Add transparency

--- init.el
+++ init.el
@@ -33,6 +33,19 @@
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 
+;; (set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
+(set-frame-parameter (selected-frame) 'alpha '(90 70))
+(add-to-list 'default-frame-alist '(alpha 85 50))
+
+(defun toggle-transparency ()
+  (interactive)
+  (if (/=
+       (cadr (frame-parameter nil 'alpha))
+       100)
+      (set-frame-parameter nil 'alpha '(100 100))
+    (set-frame-parameter nil 'alpha '(85 50))))
+(global-set-key (kbd "C-c t") 'toggle-transparency)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; General config
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
