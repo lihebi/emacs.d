4845734ab3e61730fa6b29ae7ba752f972af4766
Hebi Li <lihebi.com@gmail.com>
2015-12-25 19:44:24 -0600

    Add move-line

--- hebi-defun.el
+++ hebi-defun.el
@@ -88,5 +88,22 @@
 (global-set-key (kbd "C-c C-SPC") 'unpop-to-mark-command)
 
 
+(defun move-line-up ()
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2))
+
+(defun move-line-down ()
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1))
+
+(global-set-key (kbd "M-<up>") 'move-line-up)
+(global-set-key (kbd "M-<down>") 'move-line-down)
+
+
+
+
 (provide 'hebi-defun)
 ;;; hebi-defun.el ends here
