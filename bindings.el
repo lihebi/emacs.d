9f748bc274ad221b37be44a40cfb56146c503249
Hebi Li <lihebi.com@gmail.com>
2016-01-02 12:13:15 -0600

    Rearange bindings.el; add dired-up binding.

--- bindings.el
+++ bindings.el
@@ -3,6 +3,10 @@
 
 ;;; Code:
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; appearance
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "C-x O")
@@ -10,15 +14,10 @@
                   (interactive)
                   (other-window -1)))
 
-;; Start eshell or switch to it if it's active.
-(global-set-key (kbd "C-x m") 'eshell)
 
-;; Start a new eshell even if one is active.
-(global-set-key (kbd "C-x M")
-                (lambda () (interactive) (eshell t)))
-
-;; Start a regular shell if you prefer that.
-(global-set-key (kbd "C-x M-m") 'shell)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; efficiency
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; kill lines backward
 (global-set-key (kbd "C-<backspace>")
@@ -27,43 +26,28 @@
                   (kill-line 0)
                   (indent-according-to-mode)))
 
-(global-set-key (kbd "s-<return>")
-                (lambda()
-                  (interactive)
-                  (move-end-of-line 1)
-                  (newline)))
-
-(global-set-key (kbd "S-s-<return>")
-                (lambda()
-                  (interactive)
-                  (move-end-of-line 0)
-                  (newline)))
-
-;; backspace
-;; (global-set-key (kbd "C-h")
-;;                 (lambda ()
-;;                   (interactive)
-;;                   (backward-delete-char 1)))
-
-(global-set-key (kbd "<f1>") 'help-command)
-
 ;; join line
 (global-set-key (kbd "C-j")
                 (lambda()
                   (interactive)
                   (join-line -1)))
 
-;; other window
-(global-set-key (kbd "C-<return>") 'other-window)
-(global-set-key (kbd "<f5>") (lambda ()
-                               (interactive)
-                               (shell-command "make")
-                               ))
-
+;; when split window right, swith to that window
 (global-set-key (kbd "C-x 3") (lambda ()
                                 (interactive)
                                 (split-window-right)
                                 (other-window 1)
                                 ))
+
+;; switch between source and header file
+(global-set-key (kbd "C-x C-o") 'ff-find-other-file)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Mode specific key bindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(eval-after-load 'dired
+  '(define-key dired-mode-map (kbd "C-u") 'dired-up-directory))
 
 ;;; bindings.el ends here
