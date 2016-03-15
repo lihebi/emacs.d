019d1c90273de174527cb2acf4fd830e3bf3c14f
Hebi Li <lihebi.com@gmail.com>
2016-03-15 12:10:34 -0500

    window-system predicate for font

--- init.el
+++ init.el
@@ -151,10 +151,12 @@
 ;; http://baohaojun.github.io/perfect-emacs-chinese-font.html
 ;; https://github.com/tumashu/chinese-fonts-setup
 
-(set-face-attribute 'default nil :font "Source Code Pro")
-(set-fontset-font (frame-parameter nil 'font)
-                  'han (font-spec :family "司马彦简行修正版"
-                                  ))
+(when window-system
+  (set-face-attribute 'default nil :font "Source Code Pro")
+  (set-fontset-font (frame-parameter nil 'font)
+                    'han (font-spec :family "司马彦简行修正版"
+                                    ))
+  )
 
 
 
