8fa1cfd53d2f8ec178626d4abc59c5508593225a
Hebi Li <lihebi.com@gmail.com>
2016-08-22 20:02:50 -0500

    export black color when dark theme

--- org-conf.el
+++ org-conf.el
@@ -12,6 +12,21 @@
   :defer t
   )
 
+(defun my/org-inline-css-hook (exporter)
+  "Insert custom inline css to automatically set the
+background of code to whatever theme I'm using's background"
+  (when (eq exporter 'html)
+    (let* ((my-pre-bg (face-background 'default))
+           (my-pre-fg (face-foreground 'default)))
+      (setq
+       org-html-head-extra
+       (concat
+        org-html-head-extra
+        (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                my-pre-bg my-pre-fg))))))
+
+(add-hook 'org-export-before-processing-hook 'my/org-inline-css-hook)
+
 (use-package org
   :init
   (setq org-plain-list-ordered-item-terminator '?.) ; remove using ?) causing a listing
