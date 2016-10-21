949796dbca6df9372b847e1d69fa0914836d0a97
Hebi Li <lihebi.com@gmail.com>
2016-10-20 20:58:35 -0500

    package initialize?

--- init.el
+++ init.el
@@ -4,6 +4,13 @@
 
 ;;; Code:
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun emacs-d (filename)
   "Expand FILENAME relative to `user-emacs-directory'."
   (expand-file-name filename user-emacs-directory))
