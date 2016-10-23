06a88edc45c7c8cd127a981d4f63299a0e313a8d
Hebi Li <lihebi.com@gmail.com>
2016-10-23 11:11:44 -0500

    magic and email

--- packages.el
+++ packages.el
@@ -362,13 +362,10 @@
 (use-package regex-tool
   :defer t)
 
-(if (and (>= emacs-major-version 24)
-	     ;; FIXME 25.x
-	     (>= emacs-minor-version 4))
-  (use-package magit
-    :defer t
-    :bind (
-     ("C-x g" . magit-status))))
+(use-package magit
+  :defer t
+  :bind (
+         ("C-x g" . magit-status)))
 
 (use-package ag
   :defer t
