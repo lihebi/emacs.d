e9a1d5410d404a4b15e04e0de6528616175b2ada
Hebi Li <lihebi.com@gmail.com>
2017-02-16 11:38:02 -0600

    do not track projectile automatically

--- packages.el
+++ packages.el
@@ -376,6 +376,9 @@
     )
   :config
   (setq projectile-switch-project-action 'projectile-dired)
+  (setq projectile-track-known-projects-automatically nil)
+  ;; when setting this to another file, emacs didn't load it
+  ;; (setq projectile-known-projects-file "/home/hebi/.emacs.d/projectile-bookmarks.eld")
   )
 
 (use-package helm-projectile
