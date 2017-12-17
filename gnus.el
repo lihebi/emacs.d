cb11c48af76f116a481d7e410026fc40800283b8
Hebi Li <lihebi.com@gmail.com>
2017-12-17 13:53:28 -0600

    configure gnus for gmail

--- gnus.el
+++ gnus.el
@@ -4,55 +4,51 @@
 
 ;;; Code:
 
-
-(defvar gnus-default-nntp-server)
-(defvar gnus-select-method)
-(defvar gnus-use-adaptive-scoring)
-(defvar gnus-secondary-select-methods)
-
-
 ;; gmail
 
-(setq user-mail-address "lihebi.emacs@gmail.com"
+(setq user-mail-address "lihebi.com@gmail.com"
       user-full-name "Hebi Li")
 
-;; (setq gnus-select-method
-;;       '(nnimap "gmail"
-;; 	       (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
-;; 	       (nnimap-server-port "imaps")
-;; 	       (nnimap-stream ssl)))
-
-;; (setq smtpmail-smtp-server "smtp.gmail.com"
-;;       smtpmail-smtp-service 587
-;;       gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
-
+(setq gnus-select-method
+      '(nnimap "gmail"
+	       (nnimap-address "imap.gmail.com")  ; it could also be imap.googlemail.com if that's your server.
+	       (nnimap-server-port "imaps")
+	       (nnimap-stream ssl)))
 
 ;; send mail using gmail smtp, no require for installation of sendmail or something
 (setq message-send-mail-function 'smtpmail-send-it
       smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "lihebi.emacs@gmail.com" nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "lihebi.com@gmail.com" nil))
       smtpmail-default-smtp-server "smtp.gmail.com"
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
       starttls-use-gnutls t)
+
+;; make them always visible
+(setq gnus-permanently-visible-groups "INBOX\\|nnfolder\\+archive.*\\|nndraft:drafts")
+(setq gnus-thread-sort-functions
+           '((not gnus-thread-sort-by-number)))
 
 ;; gmane
 
-(setq gnus-select-method
-      '(nntp "news.gmane.org"))
+;; (setq gnus-select-method
+;;       '(nntp "news.gmane.org"))
 
 ;; (add-to-list 'gnus-secondary-select-methods '(nntp "news.gmane.org"))
 ;; (setq gnus-default-nntp-server "news.gmane.org")
-(setq gnus-use-adaptive-scoring t)
-(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
-(add-hook 'gnus-group-mode-hook 'hl-line-mode)
-(defvar gnus-read-newsrc-file)
-(defvar gnus-save-newsrc-file)
-(setq gnus-read-newsrc-file nil)
-(setq gnus-save-newsrc-file nil)
 
-(defvar gnus-summary-mode-hook)
-(setq gnus-summary-mode-hook 'hl-line-mode)
+
+;; (setq gnus-use-adaptive-scoring t)
+;; (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+;; (add-hook 'gnus-group-mode-hook 'hl-line-mode)
+;; (defvar gnus-read-newsrc-file)
+;; (defvar gnus-save-newsrc-file)
+;; (setq gnus-read-newsrc-file nil)
+;; (setq gnus-save-newsrc-file nil)
+
+;; (defvar gnus-summary-mode-hook)
+;; (setq gnus-summary-mode-hook 'hl-line-mode)
 
 ;; (setq gnus-summary-line-format "%U%R %5N %6k %24&user-date; â”‚ %~(max-right 75)~(pad-right 75)S â”‚ %B %f
 ;; ")
@@ -65,40 +61,40 @@
 ;; from https://eschulte.github.io/emacs-starter-kit/starter-kit-gnus.html
 ;;; bbdb
 
-(use-package bbdb
-  :init
-  ;; (require 'bbdb-autoloads)
-  (setq
-   bbdb-file "~/.bbdb"
-   bbdb-offer-save 'auto
-   bbdb-notice-auto-save-file t
-   bbdb-expand-mail-aliases t
-   bbdb-canonicalize-redundant-nets-p t
-   bbdb-always-add-addresses t
-   bbdb-complete-name-allow-cycling t
-   ))
+;; (use-package bbdb
+;;   :init
+;;   ;; (require 'bbdb-autoloads)
+;;   (setq
+;;    bbdb-file "~/.bbdb"
+;;    bbdb-offer-save 'auto
+;;    bbdb-notice-auto-save-file t
+;;    bbdb-expand-mail-aliases t
+;;    bbdb-canonicalize-redundant-nets-p t
+;;    bbdb-always-add-addresses t
+;;    bbdb-complete-name-allow-cycling t
+;;    ))
 
 ;; http://groups.google.com/group/gnu.emacs.gnus/browse_thread/thread/a673a74356e7141f
-(when window-system
-  (setq gnus-sum-thread-tree-indent "  ")
-  (setq gnus-sum-thread-tree-root "") ;; "● ")
-  (setq gnus-sum-thread-tree-false-root "") ;; "◯ ")
-  (setq gnus-sum-thread-tree-single-indent "") ;; "◎ ")
-  (setq gnus-sum-thread-tree-vertical        "│")
-  (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
-  (setq gnus-sum-thread-tree-single-leaf     "╰─► "))
-(setq gnus-summary-line-format
-      (concat
-       "%0{%U%R%z%}"
-       "%3{│%}" "%1{%&user-date;%}" "%3{│%}" ;; date
-       "  "
-       "%4{%-20,20f%}"               ;; name
-       "  "
-       "%3{│%}"
-       " "
-       "%1{%B%}"
-       "%s\n"))
-(setq gnus-summary-display-arrow t)
+;; (when window-system
+;;   (setq gnus-sum-thread-tree-indent "  ")
+;;   (setq gnus-sum-thread-tree-root "") ;; "● ")
+;;   (setq gnus-sum-thread-tree-false-root "") ;; "◯ ")
+;;   (setq gnus-sum-thread-tree-single-indent "") ;; "◎ ")
+;;   (setq gnus-sum-thread-tree-vertical        "│")
+;;   (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+;;   (setq gnus-sum-thread-tree-single-leaf     "╰─► "))
+;; (setq gnus-summary-line-format
+;;       (concat
+;;        "%0{%U%R%z%}"
+;;        "%3{│%}" "%1{%&user-date;%}" "%3{│%}" ;; date
+;;        "  "
+;;        "%4{%-20,20f%}"               ;; name
+;;        "  "
+;;        "%3{│%}"
+;;        " "
+;;        "%1{%B%}"
+;;        "%s\n"))
+;; (setq gnus-summary-display-arrow t)
 
 
 
