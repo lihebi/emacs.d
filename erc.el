024b4bbfb833744b519b8ef713bb666fdbbe7a02
Hebi Li <lihebi.com@gmail.com>
2017-12-16 21:58:57 -0600

    cleanup erc autojoin channels (I'm still not using ERC)

--- erc.el
+++ erc.el
@@ -5,26 +5,35 @@
 ;;; Code:
 
 (erc-autojoin-mode 1)
-(setq erc-autojoin-channels-alist
-      '(("freenode.net"
-         "#emacs" "#latex" "#scheme" "#haskell"
-         "#clojure" "#overtone" "#debian"
-         ;; "#gnustep" "#screen" "#freestream"
-         ;; "#fnr" "#fnr-staff" "#ducttape" "#carvux" "#unit-e" "#isys"
-         ;; "#fsptb"
-         )
-        ;; ("crystalia.net" "#crystalia")
-        ))
+
+
+;; (setq erc-autojoin-channels-alist
+;;       '(("freenode.net"
+;;          ;; "#linux"
+;;          ;; "#emacs"
+;;          ;; "#latex"
+;;          ;; "#scheme"
+;;          ;; "#haskell"
+         
+;;          ;; "#clojure"
+;;          ;; "#overtone"
+;;          ;; "#debian"
+;;          ;; "#gnustep" "#screen" "#freestream"
+;;          ;; "#fnr" "#fnr-staff" "#ducttape" "#carvux" "#unit-e" "#isys"
+;;          ;; "#fsptb"
+;;          )
+;;         ;; ("crystalia.net" "#crystalia")
+;;         ))
 
 ;; track activities
 (erc-track-mode t)
-
 (erc-fill-mode t)
 (erc-ring-mode t)
 (erc-netsplit-mode t)
 (erc-timestamp-mode t)
-(setq erc-timestamp-format "[%R-%m/%d]")
 
-(setq erc-user-full-name "Hebi Li")
+;; (setq erc-timestamp-format "[%R-%m/%d]")
+
+;; (setq erc-user-full-name "Hebi Li")
 
 ;;; erc.el ends here
