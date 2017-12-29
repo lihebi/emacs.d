a267966137ea6c9f66f9f542f4e4494c8b6c561f
Hebi Li <lihebi.com@gmail.com>
2017-12-29 01:33:26 -0600

    fontify org faces

--- org-conf.el
+++ org-conf.el
@@ -441,7 +441,32 @@
   
   ;; (use-package org-plus-contrib)
   ;; highlight
-  (setq org-src-fontify-natively t))
+  (setq org-src-fontify-natively t)
+  ;; seems not working
+  (setq org-fontify-whole-heading-line t)
+
+  ;; (ol1 '(:height 1.3 :weight bold :overline "#A7A7A7" :foreground "#3C3C3C" :background "#F0F0F0"))
+  ;; (ol2 '())
+  ;; (ol3 '())
+  ;; (ol4 '())
+  ;; (ol5 '(:height 1.0 :weight bold :slant normal :foreground "#E3258D"))
+  ;; (ol6 '(:height 1.0 :weight bold :slant italic :foreground "#0077CC"))
+
+  (set-face-attribute 'org-level-1 nil
+                      :height 1.3 :weight 'bold :overline "#A7A7A7" :foreground "#3C3C3C" :background "#F0F0F0")
+  (set-face-attribute 'org-level-2 nil
+                      :height 1.0 :weight 'bold :overline "#123555" :foreground "#123555" :background "#E5F4FB")
+  (set-face-attribute 'org-level-3 nil
+                      :height 1.0 :weight 'bold :foreground "#005522" :background "#EFFFEF")
+  (set-face-attribute 'org-level-4 nil
+                      :height 1.0 :weight 'bold :slant 'normal :foreground "#EA6300")
+
+  (set-face-attribute 'org-block nil :inherit 'shadow :background "#FFFFE0")
+  (set-face-attribute 'org-block-begin-line nil :inherit 'org-meta-line
+                      :underline "#A7A6AA" :foreground "#555555" :background "#E2E1D5")
+  (set-face-attribute 'org-block-end-line nil :inherit 'org-meta-line
+                      :overline "#A7A6AA" :foreground "#555555" :background "#E2E1D5")
+  )
 
 
 (provide 'org-conf)
