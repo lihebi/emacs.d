93831e693022610daf93fd161079fc5db74af702
Hebi Li <lihebi.com@gmail.com>
2018-03-15 21:45:20 -0500

    add eim-wubi

--- init.el
+++ init.el
@@ -62,6 +62,31 @@ manually load it."
 ;; (load "~/.emacs.d/theme.el")
 
 
+
+;; wubi
+;; setup: https://github.com/wenbinye/emacs-eim
+;; set-input-method, eim-wb
+;;
+;; starting from z, use pinyin as input and show wb code
+;; eim-describe-char show code at point
+
+(add-to-list 'load-path "~/.emacs.d/emacs-eim")
+(autoload 'eim-use-package "eim" "Another emacs input method")
+;; Tooltip 暂时还不好用
+(setq eim-use-tooltip nil)
+
+(register-input-method
+ "eim-wb" "euc-cn" 'eim-use-package
+ "五笔" "汉字五笔输入法" "wb.txt")
+(register-input-method
+ "eim-py" "euc-cn" 'eim-use-package
+ "拼音" "汉字拼音输入法" "py.txt")
+
+;; 用 ; 暂时输入英文
+(require 'eim-extra)
+(global-set-key ";" 'eim-insert-ascii)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Configuration
 
