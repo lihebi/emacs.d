30c70f65db2123f6015ed8bd75123f5a36a6c1cf
Hebi Li <lihebi.com@gmail.com>
2018-09-28 10:13:33 -0500

    init drill

---
+++ drill.el
@@ -0,0 +1,7 @@
+;; Space repetition flashcard program
+;;
+;; Related programs
+;; - https://orgmode.org/worg/org-contrib/org-drill.html
+;; - https://en.wikipedia.org/wiki/Spaced_repetition
+;; - https://github.com/dae/anki
+;; - https://www.supermemo.com/en
