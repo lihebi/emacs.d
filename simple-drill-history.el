e743e499fee7a040fd305eb0db7c028fbdc6db76
Hebi Li <lihebi.com@gmail.com>
2019-01-13 16:37:37 -0600

    simple drill history

---
+++ simple-drill-history.el
@@ -0,0 +1,24 @@
+("pretty"
+ (trans "漂亮" note nil date
+        (23611 48488 731125 454000)
+        score 2 level 4)
+ "old"
+ (trans "旧" note nil date
+        (23611 48478 253123 864000)
+        score 2 level 3)
+ "new"
+ (trans "新" note nil date
+        (23611 48479 563857 502000)
+        score 2 level 1)
+ "bad"
+ (trans "坏" note nil date
+        (23611 48477 684632 115000)
+        score 2 level 3)
+ "good"
+ (trans "好" note nil date
+        (23611 48487 644656 439000)
+        score 2 level 3)
+ "hi"
+ (trans "嗨" note nil date
+        (23611 48488 216551 61000)
+        score 2 level 3))
