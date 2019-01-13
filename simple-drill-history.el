003c7c70522aacaf99679461d73d6b359ad450bc
Hebi Li <lihebi.com@gmail.com>
2019-01-13 17:38:23 -0600

    adding new words

--- simple-drill-history.el
+++ simple-drill-history.el
@@ -21,4 +21,72 @@
  "hi"
  (trans "嗨" note nil date
         (23611 48488 216551 61000)
-        score 2 level 3))
+        score 2 level 3)
+ "prominence"
+ (trans "突出物" note nil date
+        (23611 48608 164562 630000)
+        score 0 level 0)
+ "prevailing"
+ (trans "优势" note nil date
+        (23611 48656 922122 730000)
+        score 0 level 0)
+ "commensurate"
+ (trans "相称" note nil date
+        (23611 49499 73817 945000)
+        score 0 level 0)
+ "putative"
+ (trans "假定" note nil date
+        (23611 49589 157281 294000)
+        score 0 level 0)
+ "meteorological"
+ (trans "气象" note nil date
+        (23611 49646 221734 65000)
+        score 0 level 0)
+ "auspicious"
+ (trans "吉祥" note nil date
+        (23611 49674 260939 637000)
+        score 0 level 0)
+ "arthropod"
+ (trans "节肢动物" note nil date
+        (23611 49728 380691 773000)
+        score 0 level 0)
+ "fauna"
+ (trans "动物区系" note nil date
+        (23611 49735 682705 611000)
+        score 0 level 0)
+ "aerodynamic"
+ (trans "空气动力学的" note nil date
+        (23611 49782 284337 812000)
+        score 0 level 0)
+ "alight"
+ (trans "落" note nil date
+        (23611 49817 128420 602000)
+        score 0 level 0)
+ "sporadically"
+ (trans "偶尔" note nil date
+        (23611 51166 181849 312000)
+        score 0 level 0)
+ "breeze"
+ (trans "微风" note nil date
+        (23611 51492 545920 751000)
+        score 0 level 0)
+ "insulator"
+ (trans "绝缘子" note nil date
+        (23611 51933 944206 185000)
+        score 0 level 0)
+ "triboelectric"
+ (trans "摩擦" note nil date
+        (23611 51956 855497 718000)
+        score 0 level 0)
+ "surmise"
+ (trans "推测" note nil date
+        (23611 52012 108986 533000)
+        score 0 level 0)
+ "arena"
+ (trans "竞技场" note nil date
+        (23611 52068 734781 268000)
+        score 0 level 0)
+ "misty"
+ (trans "蒙蒙" note nil date
+        (23611 52093 787683 989000)
+        score 0 level 0))
