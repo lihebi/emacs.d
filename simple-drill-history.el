bf07e4c3beece41a6ba223f413e6f0b5fcd306b1
Hebi Li <lihebi.com@gmail.com>
2019-01-17 14:32:27 -0600

    drill history

--- simple-drill-history.el
+++ simple-drill-history.el
@@ -89,4 +89,60 @@
  "misty"
  (trans "蒙蒙" note nil date
         (23611 52093 787683 989000)
+        score 0 level 0)
+ "strand"
+ (trans "缕" note nil date
+        (23611 52764 471619 282000)
+        score 0 level 0)
+ "centrifugal"
+ (trans "离心" note nil date
+        (23611 52811 269897 451000)
+        score 0 level 0)
+ "butler"
+ (trans "男管家" note nil date
+        (23611 53380 759382 125000)
+        score 0 level 0)
+ "villain"
+ (trans "恶棍" note nil date
+        (23612 9309 302039 787000)
+        score 0 level 0)
+ "cockpit"
+ (trans "座舱" note nil date
+        (23612 9719 541557 63000)
+        score 0 level 0)
+ "inertial"
+ (trans "惯性的" note nil date
+        (23612 11162 448051 846000)
+        score 0 level 0)
+ "pinch"
+ (trans "捏" note nil date
+        (23612 12559 190087 720000)
+        score 0 level 0)
+ "galvanic"
+ (trans "电流的" note nil date
+        (23614 20709 476991 131000)
+        score 0 level 0)
+ "infrared"
+ (trans "红外线" note nil date
+        (23614 27223 929842 450000)
+        score 0 level 0)
+ "dismal"
+ (trans "惨淡" note nil date
+        (23616 535 295351 405000)
+        score 0 level 0)
+ "nibble"
+ (trans "蚕食" note nil date
+        (23616 710 128261 66000)
+        score 0 level 0)
+ "primordial"
+ (trans "原始" note nil date
+        (23616 3228 472829 691000)
+        score 0 level 0)
+ "proclaim"
+ (trans "宣布" note nil date
+        (23616 54345 660900 709000)
+        score 0 level 0)
+ "heirloom"
+ (trans "传家宝" note nil date
+        (23616 57187 976466 380000)
         score 0 level 0))
